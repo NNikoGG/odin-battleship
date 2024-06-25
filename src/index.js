65bdc516ae9497badafcce36310241edc8742719
niko-linux <geekgaming222@gmail.com>
2024-06-25 19:50:27 +0530

    Add css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './style.css';
-
-let helloWorld = document.createElement('div');
-helloWorld.innerText = 'Hello World!';
-helloWorld.classList.add('hello');
-
-document.body.appendChild(helloWorld);
